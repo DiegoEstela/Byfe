558b98ff0c8bbb0c450dfc5bc4ffe66195b3d4e3
DiegoEstela <die.estela@gmail.com>
2022-05-11 17:01:14 -0300

    add navbar

---
+++ pages/components/navBar/index.style.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const NavBar = styled.nav`
+    background-color: #1865a5;
+    color: #fff;
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    border-radius: 0.25rem;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    font-size: 1.2rem;
+
+    display: flex;
+    `
+
+export const NavBar_ul = styled.ul`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 8px 40px;
+
+    `
+
+export const NavBar_li = styled.li`
+    display: flex;
+    :hover{
+        
+
+    }
+
+    `
