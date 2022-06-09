7dbc2f18585c5594e2b36bb5060ad4ec212fc0ca
DiegoEstela <die.estela@gmail.com>
2022-06-09 15:52:55 -0300

    fix navbar

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -1,26 +1,34 @@
-import * as styles from "./index.style";
-import Image from "next/image";
 import Link from "next/link";
-import style from "../../styles/navBar.module.css"
+import {useState} from "react"
+import { RiSendPlane2Line} from "react-icons/ri";
 
 export default function NavBar() {
+  const [active, setActive] = useState(false)
   return (
 
-    <div className={style.header}>
-      <styles.Logo>
-        <Image src="/logoByFe.png" alt="byfe" width={80} height={80} />
-      </styles.Logo>
-     
-     
-        <styles.Nav active={true}>
-      <styles.Nav_li > <Link href="/">HOME  </Link></styles.Nav_li>
-      <styles.Nav_li >ABOUT AS</styles.Nav_li>
-      <styles.Nav_li >SERVICES</styles.Nav_li>
-      <styles.Nav_li >CONTACT US</styles.Nav_li>
+    <div className="header">
+      <a className="logo">
+        <h2 className="title">Byfe</h2>
+      </a>
 
-        </styles.Nav>
-        <styles.ClearFix> </styles.ClearFix>
-      
+      <div className={active ? "menu-toggle-open" : "menu-toggle" } 
+    
+      onClick={()=> setActive(!active)}> 
+      <RiSendPlane2Line/> 
+     
+    
+      </div>
+     
+        <nav className={active ? "active" : ""}>
+      <a>  <Link href="/">HOME  </Link></a>
+      <a > <Link href="/">ABOUT US  </Link></a>
+      <a > <Link href="/">SERVICES  </Link></a>
+      <a><Link href="/">CONTACT US  </Link></a>  
+      <div className="animation start"> </div>
+      </nav>
+
+        <div className="clearFix" > </div>
+
     </div>
   );
 }
