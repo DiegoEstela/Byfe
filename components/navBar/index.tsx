3dca6efe58fbfe9035d7310949f3b8796e2d5966
DiegoEstela <die.estela@gmail.com>
2022-06-09 15:55:38 -0300

    close navbar

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -1,34 +1,43 @@
 import Link from "next/link";
-import {useState} from "react"
-import { RiSendPlane2Line} from "react-icons/ri";
+import { useState } from "react";
+import { RiSendPlane2Line } from "react-icons/ri";
 
 export default function NavBar() {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
   return (
-
-    <div className="header">
+    <div className="header"
+    onClick={() => setActive(!active)}>
       <a className="logo">
         <h2 className="title">Byfe</h2>
       </a>
 
-      <div className={active ? "menu-toggle-open" : "menu-toggle" } 
-    
-      onClick={()=> setActive(!active)}> 
-      <RiSendPlane2Line/> 
-     
-    
+      <div
+        className={active ? "menu-toggle-open" : "menu-toggle"}
+        onClick={() => setActive(!active)}
+      >
+        <RiSendPlane2Line />
       </div>
-     
-        <nav className={active ? "active" : ""}>
-      <a>  <Link href="/">HOME  </Link></a>
-      <a > <Link href="/">ABOUT US  </Link></a>
-      <a > <Link href="/">SERVICES  </Link></a>
-      <a><Link href="/">CONTACT US  </Link></a>  
-      <div className="animation start"> </div>
+
+      <nav className={active ? "active" : ""}>
+        <a>
+          {" "}
+          <Link href="/">HOME </Link>
+        </a>
+        <a>
+          {" "}
+          <Link href="/">ABOUT US </Link>
+        </a>
+        <a>
+          {" "}
+          <Link href="/">SERVICES </Link>
+        </a>
+        <a>
+          <Link href="/">CONTACT US </Link>
+        </a>
+        <div className="animation start"> </div>
       </nav>
 
-        <div className="clearFix" > </div>
-
+      <div className="clearFix"> </div>
     </div>
   );
 }
